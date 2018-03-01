58cd7b3c34ae9548cfcc47f2635572beb8c45aa6
gaetanV <gaetan.vigneron@gmail.com>
2018-03-01 15:59:29 +0100

    Config Tasks Dispatcher

---
+++ dispacher/boot.sh
@@ -0,0 +1,10 @@
+$KAFKA_HOME/bin/zookeeper-server-start.sh $KAFKA_HOME/config/zookeeper.properties 2>&1 &
+$KAFKA_HOME/bin/kafka-server-start.sh $KAFKA_HOME/config/server.properties 2>&1 &
+
+sleep 4
+
+$KAFKA_HOME/bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic symfonyTasks
+$KAFKA_HOME/bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic symfonyStats
+
+$KAFKA_HOME/bin/kafka-topics.sh --list --zookeeper localhost:2181
+
